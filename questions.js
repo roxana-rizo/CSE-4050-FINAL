9e03bac66db5f488749a77f963949ea5418efb54
sm483790-max <sm483790@gmail.com>
2025-11-24 16:29:19 -0800

    Created questions.js

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Type Multi Language",
+            "Hyper Text Multiple Language",
+            "Hyper Text Markup Language",
+            "Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Cascading Style Sheet",
+            "Cute Style Sheet",
+            "Computer Style Sheet",
+            "Codehal Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hometext Programming",
+            "Hypertext Preprogramming",
+            "Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Strength Query Language",
+            "Stylesheet Query Language",
+            "Science Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "Extensible Markup Language",
+        options: [
+            "Excellent Multiple Language",
+            "Explore Multiple Language",
+            "Extra Markup Language",
+            "Extensible Markup Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which character is used to indicate an end tag?",
+        answer: "/",
+        options: [
+            "<",
+            "*",
+            "/",
+            "^"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which JavaScript keyword is used to declare a variable?",
+        answer: "let",
+        options: [
+            "varable",
+            "int",
+            "let",
+            "define"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Which symbol is used for comments in JavaScript?",
+        answer: "//",
+        options: [
+            "//",
+            "#",
+            "/* */",
+            "**"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which of these is a correct way to create a function in JavaScript?",
+        answer: "function myFunction() {}",
+        options: [
+            "function:myFunction() {}",
+            "function myFunction() {}",
+            "create function myFunction() {}",
+            "function = myFunction() {}"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which operator is used for strict equality in JavaScript?",
+        answer: "===",
+        options: [
+            "==",
+            "=",
+            "!==",
+            "===" 
+        ]
+    }
+];
